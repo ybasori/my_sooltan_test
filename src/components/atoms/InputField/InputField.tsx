65b44936c357b7ba9b5e78674d0fea3753f9c8d1
yusuf <ybasori@gmail.com>
2023-01-25 05:36:31 +0700

    input field component

---
+++ src/components/atoms/InputField/InputField.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./InputField.module.scss";
+
+const InputField: React.FC<{
+  type?: "text" | "password" | "number";
+  value: string;
+  placeholder: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}> = ({ type = "text", value, onChange, placeholder }) => {
+  return (
+    <input
+      type={type}
+      className={`${styles["input"]} ${
+        value !== "" ? styles["not-empty"] : ""
+      }`}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default InputField;
